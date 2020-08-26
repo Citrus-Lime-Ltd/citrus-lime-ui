1b15a9be0fefe94f2dd214f9552b1c1a808785bc
bjkippax <36036691+bjkippax@users.noreply.github.com>
2020-08-26 09:20:23 +0100

    added JEST unit testing

---
+++ src/types/Core/String.extensions.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    interface String {
+        Similarity(comparisonString: string): number;
+        TrimChar(character: string): string;
+        TrimStartChar(character: string): string;
+        TrimEndChar(character: string): string;
+        TrimToLength(length: number, addElipsis: boolean): string;
+        CamelCaseToSentence(): string;
+        ToTitleCase(): string;
+    }
+}
+
+export {}
---
+++ tests/utils-extensions.test.ts
@@ -0,0 +1,26 @@
+import 'jest';
+import '../src/utils/extensions/String';
+
+describe('Test String Extensions', () => {
+
+    test('Test camelcase to sentence case', () => {
+        expect('iAmTestingThis'.CamelCaseToSentence()).toEqual("I Am Testing This");
+    });
+
+    test('Test trim characters', () => {
+        expect('/test/'.TrimChar('/')).toEqual("test");
+    });
+
+    test('Test trim string to length (without ellipsis)', () => {
+        expect('testing'.TrimToLength(4, false)).toEqual("test");
+    });
+
+    test('Test trim string to length (with ellipsis)', () => {
+        expect('trim string to length with ellipsis'.TrimToLength(30, true)).toEqual("trim string to length with ...");
+    });
+
+    test('Test to title case', () => {
+        expect('i am testing this'.ToTitleCase()).toEqual("I Am Testing This");
+    });
+
+});
