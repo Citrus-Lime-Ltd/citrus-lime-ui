2dc4a20e499d3dc623dcfdd43942b904c656a53d
Matt Geldart <matthew@citruslime.com>
2020-08-28 12:52:56 +0100

    Further update to inital UIkit types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,4 @@
 import superGridComponent from '@/components/super-grid/SuperGrid.vue';
-import uikitCore from 'uikit';
 
 export { core, Core } from './core';
 export * from './core/StringExtensions';
@@ -9,7 +8,6 @@ export { FilterArgs, FilterRequest, SortArgs } from './FilterRequest';
 export { FilterResponse } from './FilterResponse';
 export { GridColumn } from './GridColumn';
 
-declare type UIkit = typeof uikitCore;
 export declare const uikit: UIkit;
 
 declare type SuperGrid = typeof superGridComponent;
