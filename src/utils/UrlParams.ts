66f757c60b6c59efc8f71c9f6faed6aee4b01d6c
bjkippax <36036691+bjkippax@users.noreply.github.com>
2020-08-28 17:00:55 +0100

    fix linting and build errors

--- src/utils/UrlParams.ts
+++ src/utils/UrlParams.ts
@@ -1,13 +1,13 @@
 const GetParamsByName = (stringToSearch: string, paramToFind: string): string => {
-    let valueToReturn: string = '';
+    let valueToReturn = '';
 
     const urlToSearch: string = decodeURIComponent(stringToSearch),
         regexS: string = '[\\?&]' + paramToFind + '=([^&#]*)',
-        regex: RegExp = new RegExp(regexS),
+        regex = new RegExp(regexS),
         results: RegExpExecArray | null = regex.exec(urlToSearch);
 
     if (results && results[1]) {
-        valueToReturn = decodeURIComponent(results[1].replace(/\+/g, " "));
+        valueToReturn = decodeURIComponent(results[1].replace(/\+/g, ' '));
     }
 
     return valueToReturn;
